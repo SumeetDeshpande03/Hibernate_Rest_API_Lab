a39529ebd83f80b06d3b5e94114e09d8f13fd030
ambika2494 <61039307+ambika2494@users.noreply.github.com>
2020-11-21 12:30:01 -0800

    Update Sponsor.java
    need mapping in sponsor

--- src/main/java/edu/sjsu/cmpe275/lab2/entity/Sponsor.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/entity/Sponsor.java
@@ -1,77 +1,92 @@
 package edu.sjsu.cmpe275.lab2.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Column;
 import javax.validation.constraints.NotNull;
 
 /**
- * Entity object for Sponsor. This is mapped to the Sponsor Table in the
- * database
- * 
+ * Entity object for Sponsor. This is mapped to the Sponsor Table in the database
  * @author sumeetdeshpande
  *
  */
 @Entity
 @Table(name = "SPONSOR")
 public class Sponsor {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@NotNull
+	private String name;
+	
+	private String description;
+	
+	@Embedded
+	private Address address;
+	
+	@OneToMany(targetEntity = Player.class, mappedBy = "sponsor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Player> players;
+	
+	//@JoinColumn(name = "sponsor_id_fk", referencedColumnName = "id")
 
-    @Id
-    @GeneratedValue
-    private long id;
+	public Sponsor() {
+		
+	}
+	
+	public Sponsor(String name, String description, Address address) {
+		this.name = name;
+		this.description = description;
+		this.address = address;
+	}
+	
+	public long getId() {
+		return id;
+	}
 
-    @NotNull
-    private String name;
+	public void setId(long id) {
+		this.id = id;
+	}
 
-    private String description;
+	public String getName() {
+		return name;
+	}
 
-    @Embedded
-    private Address address;
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public Sponsor() {
+	public String getDescription() {
+		return description;
+	}
 
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    // Add players
-    public Sponsor(String name, String description, Address address) {
-        this.name = name;
-        this.description = description;
-        this.address = address;
-    }
+	public Address getAddress() {
+		return address;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public List<Player> getPlayers() {
+		return players;
+	}
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
+	public void setPlayers(List<Player> players) {
+		this.players = players;
+	}
+	
 }
