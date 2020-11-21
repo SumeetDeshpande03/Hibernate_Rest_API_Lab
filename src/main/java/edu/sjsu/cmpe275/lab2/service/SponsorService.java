3f5eff693004bb243eccee817e492501abd5360f
ambika2494 <61039307+ambika2494@users.noreply.github.com>
2020-11-21 12:33:01 -0800

    Update SponsorService.java
    sponsor mapping

--- src/main/java/edu/sjsu/cmpe275/lab2/service/SponsorService.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/service/SponsorService.java
@@ -3,35 +3,27 @@ package edu.sjsu.cmpe275.lab2.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import edu.sjsu.cmpe275.lab2.entity.Player;
 import edu.sjsu.cmpe275.lab2.entity.Sponsor;
 import edu.sjsu.cmpe275.lab2.repository.SponsorRepository;
 
 @Service
 public class SponsorService {
 
-    @Autowired
-    private SponsorRepository repository;
-
-    /**
-     * Updates the Sponsor corresponding to the Sponsor id
-     * 
-     * @param id
-     * @param newSponsor
-     * @return
-     */
-    public Sponsor updateSponsorById(long id, Sponsor newSponsor) {
-        Sponsor existingSponsor = repository.findById(id).orElse(null);
-        /**
-         * Return null if Sponsor not found
-         */
-        if (existingSponsor == null) {
-            return null;
-        } else {
-            existingSponsor.setName(newSponsor.getName());
-            existingSponsor.setDescription(newSponsor.getDescription());
-            existingSponsor.setAddress(newSponsor.getAddress());
-            // Add Players
-            return repository.save(existingSponsor);
-        }
-    }
+	@Autowired
+	private SponsorRepository repository;
+	
+	public Sponsor createSponsor(Sponsor sponsor) {
+		return repository.save(sponsor);
+	}
+	
+	/**
+	 * Gets the sponsor by sponsor id
+	 * @param id
+	 * @return
+	 */
+	public Sponsor getSponsorById(long id) {
+		return repository.findById(id).orElse(null);
+	}
+	
 }
