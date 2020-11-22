0f497ad13838047f827dd3ad7d20b4fb27133eb3
ambika2494 <61039307+ambika2494@users.noreply.github.com>
2020-11-22 13:54:02 -0800

    Update SponsorRepository.java

--- src/main/java/edu/sjsu/cmpe275/lab2/repository/SponsorRepository.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/repository/SponsorRepository.java
@@ -6,7 +6,7 @@ import edu.sjsu.cmpe275.lab2.entity.Sponsor;
 
 /**
  * Interface Sponsor Repository which extends the JPA Repository for performing all the CRUD operations
- * @author sumeetdeshpande
+ * @author sumeetdeshpande, AmbikaNa
  *
  */
 public interface SponsorRepository extends JpaRepository<Sponsor, Long> {
